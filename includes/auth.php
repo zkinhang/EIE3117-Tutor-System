e049544ffcc9ed98f53b84d3f0bdd3adc403719c
sunny0275 <howing227.hh@gmail.com>
2025-04-21 19:00:36 +0800

    Update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -143,17 +143,17 @@ class Auth {
                     setcookie('remember_token', $token, [
                         'expires' => time() + (30 * 24 * 60 * 60),
                         'path' => '/',
-                        'secure' => true,
+                        'secure' => false,
                         'httponly' => true,
-                        'samesite' => 'Strict'
+                        'samesite' => 'Lax'
                     ]);
                     
                     setcookie('user_id', $user['id'], [
                         'expires' => time() + (30 * 24 * 60 * 60),
                         'path' => '/',
-                        'secure' => true
+                        'secure' => false,
                         'httponly' => true,
-                        'samesite' => 'Strict'
+                        'samesite' => 'Lax'
                     ]);
                 }
 
